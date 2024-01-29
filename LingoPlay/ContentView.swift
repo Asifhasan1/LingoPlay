9e1bbe121f64bde5d83b889336cb131149bea60a
Asif <md.mushfikhasanasif@gmail.com>
2024-01-29 16:01:36 -0500

    Initial Commit

---
+++ LingoPlay/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  LingoPlay
+//
+//  Created by Asif on 1/29/24.
+//m
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("Welcome")
+            
+                .font(.custom("LavaTelugu-Bold", size: 70))
+                .foregroundColor(Color(red: 35/255, green: 77/255, blue: 112/255))
+            
+            
+            Text("Ready to Play?")
+                .font(.custom("AkayaTelivigala-Bold", size: 30))
+                .foregroundColor(Color(red: 35/255, green: 77/255, blue: 112/255))
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+              
+            }) {
+                Spacer()
+                Text("Let's Go!")
+                    .font(.custom("AkayaTelivigala-Regular", size: 36))
+                    .foregroundColor(Color(red: 228/255, green: 235/255, blue: 207/255))
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(red: 35/255, green: 77/255, blue: 112/255))
+                    .cornerRadius(10)
+                Spacer()
+            }
+            .padding(.bottom, 40)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 228/255, green: 235/255, blue: 207/255))
+        .ignoresSafeArea()
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ LingoPlay/LingoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoPlayApp.swift
+//  LingoPlay
+//
+//  Created by Asif on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LingoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
